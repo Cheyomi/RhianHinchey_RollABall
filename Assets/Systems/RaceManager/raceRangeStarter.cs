22b9f847237c451b72aeafa9d92e0aeafab9f20b
Rhian <139021661+Cheyomi@users.noreply.github.com>
2025-12-01 00:43:15 -0400

    racers

---
+++ Assets/Systems/RaceManager/raceRangeStarter.cs
@@ -0,0 +1,28 @@
+//using UnityEngine;
+
+//public class raceRangeStarter : MonoBehaviour
+//{
+   // public bool playerInRange = false;
+
+
+   // void Update()
+    //{
+      //  if (raceTrigger.playerInRange && Input.GetKeyDown(KeyCode.E))
+      //  {
+           // OpenBetMenu();
+      //  }
+   // }
+
+  //  private void OnTriggerEnter(Collider other)
+  //  {
+     //   if (other.CompareTag("Player"))
+      //      playerInRange = true;
+   // }
+
+   // private void OnTriggerExit(Collider other)
+   // {
+    //    if (other.CompareTag("Player"))
+    //        playerInRange = false;
+ //   }
+
+//}
